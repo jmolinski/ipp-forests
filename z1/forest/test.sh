8275897c0e93a7df30dc40238ce5e21925d732ed
Jakub Molinski <jakub@molinski.dev>
2020-03-08 07:51:39 +0100

    Enabled valgrind in test string

--- z1/forest/test.sh
+++ z1/forest/test.sh
@@ -9,8 +9,8 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NOCOLOR='\033[0m'
 
-VALGRIND_ERROR_CODE=15
-VALGRIND="valgrind -q"
+VALGRIND_ERROR_CODE=33
+VALGRIND="valgrind --error-exitcode=${VALGRIND_ERROR_CODE} --leak-check=full --errors-for-leak-kinds=all -q"
 PROGRAM=$1
 DIRECTORY=$2
 
@@ -20,21 +20,21 @@ trap 'rm -f "temp_out"' INT TERM HUP EXIT
 trap 'rm -f "temp_err"' INT TERM HUP EXIT
 
 for i in $DIRECTORY/*.in ; do
-    echo "Testing ${i#*$DIRECTORY/}..."
-    cat $i | $VALGRIND $PROGRAM  2>"$temp_err" 1>"$temp_out"
-    EXIT_CODE=$?
+    echo "Test ${i#*$DIRECTORY/}..."
+    cat "$i" | $VALGRIND "$PROGRAM"  2>"$temp_err" 1>"$temp_out"
+    VALGRIND_EXIT_CODE=$?
 
-    if ((EXIT_CODE == VALGRIND_ERROR_CODE)); then
+    if ((VALGRIND_EXIT_CODE == VALGRIND_ERROR_CODE)); then
         echo -ne "${RED}Wykryto wycieki pamieci, ${NOCOLOR}"
-    elif ((EXIT_CODE == 0)); then
+    elif ((VALGRIND_EXIT_CODE == 0)); then
         echo -ne "${GREEN}Brak wyciekow pamieci, ${NOCOLOR}"
     else
-        echo -ne "${RED}Valgrind error, exit code ${EXIT_CODE}, ${NOCOLOR}"
+        echo -ne "${RED}Valgrind error, exit code ${VALGRIND_EXIT_CODE}, ${NOCOLOR}"
     fi
 
     #Output
-    if cmp -s ${i%in}out "$temp_out" ; then
-        if cmp -s ${i%in}err "$temp_err"; then
+    if cmp -s "${i%in}"out "$temp_out" ; then
+        if cmp -s "${i%in}"err "$temp_err"; then
             echo -e "${GREEN}Prawidlowe wyjscie${NOCOLOR}"
         else
             echo -e "${RED}Nieprawidlowe wyjscie na stderr${NOCOLOR}"
